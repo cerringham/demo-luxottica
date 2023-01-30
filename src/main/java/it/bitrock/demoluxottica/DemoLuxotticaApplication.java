289f32689e5a5b51ff551083f2a50b4351aede55
cerrigham <cerringham@gmail.com>
2023-01-30 20:44:44 +0100

    Initial commit

---
+++ src/main/java/it/bitrock/demoluxottica/DemoLuxotticaApplication.java
@@ -0,0 +1,13 @@
+package it.bitrock.demoluxottica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLuxotticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLuxotticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/bitrock/demoluxottica/DemoLuxotticaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.bitrock.demoluxottica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLuxotticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
