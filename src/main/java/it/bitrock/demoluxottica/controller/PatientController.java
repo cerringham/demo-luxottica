65fbc13fb2f3499c7a5436986cde333ca6dcaa22
federicogradizzi <federico.gradizzi@hotmail.com>
2023-01-31 10:45:09 +0100

    pom aggiornato

--- src/main/java/it/bitrock/demoluxottica/controller/PatientController.java
+++ src/main/java/it/bitrock/demoluxottica/controller/PatientController.java
@@ -1,19 +1,24 @@
 package it.bitrock.demoluxottica.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.bitrock.demoluxottica.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/paziente")
+@Tag(name = "Patient Controller", description = "Manage of the patient")
 public class PatientController {
 
     @Autowired
     PatientService service;
 
-    @GetMapping("/patient/{id}")
+    @GetMapping("/{id}")
     public String getPatientByStringId(@PathVariable String id) {
         return service.getPatientByStringId(id);
     }
+
 }
