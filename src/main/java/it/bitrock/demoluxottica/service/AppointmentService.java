a37b8b91a9a96ddb3720e210392777bbd4dfe8fc
federicogradizzi <federico.gradizzi@hotmail.com>
2023-02-06 09:41:27 +0100

    added method in: AppointmentController, AppointmentService

--- src/main/java/it/bitrock/demoluxottica/service/AppointmentService.java
+++ src/main/java/it/bitrock/demoluxottica/service/AppointmentService.java
@@ -4,12 +4,18 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Appointment;
+import org.hl7.fhir.r4.model.Meta;
 import org.hl7.fhir.r4.model.Reference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -49,4 +55,46 @@ public class AppointmentService {
         }
     }
 
+    public ResponseEntity<?> addAppointment() {
+        Appointment appointment = new Appointment();
+        // Set the relevant details for the appointment
+        appointment.setId("2345-3455-5555-8966");
+        LocalDateTime now = LocalDateTime.now();
+        appointment.setMeta(
+                new Meta()
+                        .setVersionId("1")
+                        .setLastUpdated(Date.from(now.atZone(ZoneId.systemDefault()).toInstant())));
+        appointment.setDescription("eye injuries");
+        appointment.setStatus(Appointment.AppointmentStatus.BOOKED);
+
+        LocalDateTime start = LocalDateTime.of(2023, 10, 22, 10, 10);
+        appointment.setStart(Date.from(start.atZone(ZoneId.systemDefault()).toInstant()));
+        LocalDateTime end = LocalDateTime.of(2023, 10, 22, 12, 10);
+        appointment.setEnd(Date.from(end.atZone(ZoneId.systemDefault()).toInstant()));
+
+        Reference patientRef = new Reference();
+        //we cannot add an appointment without an exists Patient/id
+        patientRef.setReference("Patient/example");
+        Appointment.AppointmentParticipantComponent patientAppointment =
+                new Appointment.AppointmentParticipantComponent();
+        patientAppointment.setActor(patientRef)
+                .setStatus(Appointment.ParticipationStatus.ACCEPTED);
+        appointment.addParticipant(patientAppointment);
+        appointment.addParticipant(new Appointment.AppointmentParticipantComponent()
+                .setActor(new Reference()
+                        .setDisplay("Dr Doe"))
+                .setStatus(Appointment.ParticipationStatus.ACCEPTED));
+        log.info("Appointment: {}", appointment);
+
+        for (Field field : appointment.getClass().getDeclaredFields()) {
+            log.info("field: {}, {}" + field.getName(), Arrays.stream(field.getClass().getDeclaredFields()).findAny());
+        }
+
+        boolean created = getClient(url).create().resource(appointment).execute().getCreated();
+        if(created){
+            return ResponseEntity.ok(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(appointment));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
