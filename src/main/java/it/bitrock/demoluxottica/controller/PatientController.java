804c95fbfb19c7b82f2c0c64b88efd393f60d565
federicogradizzi <federico.gradizzi@hotmail.com>
2023-02-08 17:29:32 +0100

    fixed AppointmentService

--- src/main/java/it/bitrock/demoluxottica/controller/PatientController.java
+++ src/main/java/it/bitrock/demoluxottica/controller/PatientController.java
@@ -1,10 +1,12 @@
 package it.bitrock.demoluxottica.controller;
 
+import it.bitrock.demoluxottica.models.dto.PatientDTO;
 import it.bitrock.demoluxottica.models.enumerations.FhirContextEnum;
 import it.bitrock.demoluxottica.service.PatientService;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +31,16 @@ public class PatientController {
     public Optional<Patient> getPatientByStringId(@PathVariable String id) {
         log.info("---------------------------     Called getPatientByStringId Endpoint     ---------------------------");
         return service.getPatientByStringId(id, FhirContextEnum.R4);
+    }
+
+    @GetMapping("/add_patient")
+    public ResponseEntity<?> addPatient(){
+        return service.addPatient();
+    }
+
+    @PostMapping("/add_patient")
+    public ResponseEntity<?> addPatient(PatientDTO patientDTO){
+        return service.addPatient(patientDTO);
     }
 
 }
