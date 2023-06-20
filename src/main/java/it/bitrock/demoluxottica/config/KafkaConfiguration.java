ba7e1202a7267cd76f44c851371e298b608f1afc
Romagnoli Mirco <mirco.romagnoli@bitrock.it>
2023-06-20 16:00:09 +0200

    fix kafka broker configuration

--- src/main/java/it/bitrock/demoluxottica/config/KafkaConfiguration.java
+++ src/main/java/it/bitrock/demoluxottica/config/KafkaConfiguration.java
@@ -1,22 +1,9 @@
 package it.bitrock.demoluxottica.config;
 
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import it.bitrock.demoluxottica.model.avro.PatientRecord;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.Map;
-
-@Configuration
 public class KafkaConfiguration {
     public static final String ADDED_TOPIC = "patient-added";
     public static final String REMOVED_TOPIC = "patient-removed";
@@ -46,21 +33,4 @@ public class KafkaConfiguration {
                 .build();
     }
 
-    @Bean
-    public KafkaTemplate<String, PatientRecord> kafkaTemplate(ProducerFactory<String, PatientRecord> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
-    }
-
-    @Bean
-    public ProducerFactory<String, PatientRecord> producerFactory(
-            @Value("${kafka.bootstrap-servers}") String bootstrapServers,
-            @Value("${kafka.schema.registry.url}") String schemaRegistryUrl) {
-        Map<String, Object> config = Map.of(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName(),
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName(),
-                AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl
-        );
-        return new DefaultKafkaProducerFactory<>(config);
-    }
 }
